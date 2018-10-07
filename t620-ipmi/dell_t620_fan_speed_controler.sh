c06629195e2387bc9fba5955137059a83d9a3f3f
Terence <contact@ngx.hk>
2018-10-07 19:37:56 +0800

    调整温度的获取方式与计算方式 1. 通过SNMP获取CPU1与CPU2的温度值并算出平均数； 2. 根据平均数与设定值的比较结果调整风扇转速。

--- t620-ipmi/dell_t620_fan_speed_controler.sh
+++ t620-ipmi/dell_t620_fan_speed_controler.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 ipmi_host=""
-ipmi_user="root"
+ipmi_user=""
 ipmi_passwd=""
-temp_threshold="65"
 
-get_value=`ipmitool -I lanplus -H $ipmi_host -U $ipmi_user -P $ipmi_passwd sensor reading "Temp" | cut -d "|" -f 2 | cut -d " " -f 2`
+snmp_host=""
+snmp_ver="2c"
+snmp_com="public"
 
-if [[ $get_value > $temp_threshold ]]
-  then
-    ipmitool -I lanplus -H $ipmi_host -U $ipmi_user -P $ipmi_passwd raw 0x30 0x30 0x01 0x01
-  else
-    ipmitool -I lanplus -H $ipmi_host -U $ipmi_user -P $ipmi_passwd raw 0x30 0x30 0x01 0x00
-    ipmitool -I lanplus -H $ipmi_host -U $ipmi_user -P $ipmi_passwd raw 0x30 0x30 0x02 0xff 0x1e
+temp_threshold="60"
+
+get_cpu1_temp=`snmpwalk -v $snmp_ver -c $snmp_com $snmp_host SNMPv2-SMI::enterprises.674.10892.5.4.700.20.1.6.1.2 | cut -d " " -f 4`
+get_cpu2_temp=`snmpwalk -v $snmp_ver -c $snmp_com $snmp_host SNMPv2-SMI::enterprises.674.10892.5.4.700.20.1.6.1.3 | cut -d " " -f 4`
+
+calc_avg_temp=`expr \( $get_cpu1_temp + $get_cpu2_temp \) \/ 2`
+
+# get_value=`ipmitool -I lanplus -H $ipmi_host -U $ipmi_user -P $ipmi_passwd sensor reading "Temp" | cut -d "|" -f 2 | cut -d " " -f 2`
+
+if [[ $calc_avg_temp > $temp_threshold ]]
+then
+  ipmitool -I lanplus -H $ipmi_host -U $ipmi_user -P $ipmi_passwd raw 0x30 0x30 0x01 0x01
+else
+  ipmitool -I lanplus -H $ipmi_host -U $ipmi_user -P $ipmi_passwd raw 0x30 0x30 0x01 0x00
+  ipmitool -I lanplus -H $ipmi_host -U $ipmi_user -P $ipmi_passwd raw 0x30 0x30 0x02 0xff 0x1e
 fi
