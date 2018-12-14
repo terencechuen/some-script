0313258e4248e8a9111cbc1862009d34716ae3ce
Terence Chuen <contact@ngx.hk>
2018-12-14 14:57:06 +0800

    修正检查PTR记录异常的BUG 移除测试用的内容，修复BUG。

--- ngx-pxe-boot/ngx-shell/ngx-pxe-setting-script.sh
+++ ngx-pxe-boot/ngx-shell/ngx-pxe-setting-script.sh
@@ -1,7 +1,8 @@
 #/bin/sh
 
 ######## hostname ########
-PTR_ANSWER=`dig +short -x 10.1.1.141`
+LOCAL_IPADDR=`hostname -I | cut -d ' ' -f 1`
+PTR_ANSWER=`dig +short -x $LOCAL_IPADDR`
 
 if [ -z $PTR_ANSWER ] ; then
 	hostnamectl set-hostname $(cat /sys/class/net/ens192/address | sed 's/://g' | cut -c 7-12)
